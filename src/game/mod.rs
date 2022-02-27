f7ab4d57f413370696b59edba57f0891d56cdaf8
Camillo Dell'mour <cdellmour@gmail.com>
2022-02-27 09:56:35 +0100

    re-organized files a bit. Implemented menu (surprisingly easy).

---
+++ src/game/mod.rs
@@ -0,0 +1,75 @@
+use bevy::prelude::*;
+use super::GameState;
+
+pub struct GamePlugin;
+
+impl Plugin for GamePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system_set(SystemSet::on_enter(GameState::Game).with_system(setup_game))
+            .add_system_set(SystemSet::on_update(GameState::Game).with_system(keyboard_input_system))
+            .add_system_set(
+                SystemSet::on_exit(GameState::Game).with_system(despawn_screen::<OnGameScreen>),
+            );
+    }
+}
+
+#[derive(Component)]
+struct OnGameScreen;
+
+
+fn setup_game(mut commands: Commands,
+              mut materials: ResMut<Assets<StandardMaterial>>,
+              asset_server: Res<AssetServer>) {
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    }).insert(OnGameScreen);
+    // camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    }).insert(OnGameScreen);
+
+    // whale
+    // Load OBJ file
+    let whale_mesh_handle = asset_server.load("models/whale.obj");
+    commands.spawn_bundle(PbrBundle {
+        mesh: whale_mesh_handle,
+        material: materials.add(StandardMaterial {
+            base_color_texture: Some(asset_server.load("models/whale.png")),
+            ..Default::default()
+        }),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0)
+            .with_rotation(Quat::from_rotation_y(1.)),
+        ..Default::default()
+    }).insert(OnGameScreen);
+
+    // barnacle
+    // Load OBJ file
+    let barnacle_mesh_handle = asset_server.load("models/barnacle.obj");
+    commands.spawn_bundle(PbrBundle {
+        mesh: barnacle_mesh_handle,
+        material: materials.add(Color::rgb(0.2, 0.2, 0.9).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 2.0).with_scale(Vec3::new(0.2, 0.2, 0.2)),
+        ..Default::default()
+    }).insert(OnGameScreen);
+}
+
+fn keyboard_input_system(mut game_state: ResMut<State<GameState>>, keyboard_input: Res<Input<KeyCode>>) {
+    if keyboard_input.pressed(KeyCode::Escape) {
+        let _ = game_state.set(GameState::Menu);
+    }
+}
+
+// Generic system that takes a component as a parameter, and will despawn all entities with that component
+fn despawn_screen<T: Component>(to_despawn: Query<Entity, With<T>>, mut commands: Commands) {
+    for entity in to_despawn.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
+}
---
+++ src/menu/mod.rs
@@ -0,0 +1,273 @@
+//adapted from menu example https://github.com/bevyengine/bevy/blob/main/examples/game/game_menu.rs
+
+use bevy::prelude::*;
+use bevy::app::AppExit;
+use super::GameState;
+
+const TEXT_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
+
+pub struct MenuPlugin;
+
+impl Plugin for MenuPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_state(MenuState::Disabled)
+            .add_system_set(SystemSet::on_enter(GameState::Menu).with_system(menu_setup))
+            .add_system_set(SystemSet::on_enter(MenuState::Main).with_system(main_menu_setup))
+            .add_system_set(
+                SystemSet::on_exit(MenuState::Main)
+                    .with_system(despawn_screen::<OnMainMenuScreen>),
+            )
+            .add_system_set(
+                SystemSet::on_enter(MenuState::Settings).with_system(settings_menu_setup),
+            )
+            .add_system_set(
+                SystemSet::on_exit(MenuState::Settings)
+                    .with_system(despawn_screen::<OnSettingsMenuScreen>),
+            )
+            .add_system_set(
+                SystemSet::on_update(GameState::Menu)
+                    .with_system(menu_action)
+                    .with_system(button_system),
+            )
+            .add_system_set(SystemSet::on_exit(GameState::Menu)
+                    .with_system(despawn_screen::<MainMenuState>));
+    }
+}
+// State used for the current menu screen
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+enum MenuState {
+    Main,
+    Settings,
+    Disabled,
+}
+
+// Tag component used to tag entities added on the main menu screen
+#[derive(Component)]
+struct OnMainMenuScreen;
+
+// Tag component used to tag entities added on the main menu screen
+#[derive(Component)]
+struct MainMenuState;
+
+// Tag component used to tag entities added on the settings menu screen
+#[derive(Component)]
+struct OnSettingsMenuScreen;
+
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+const HOVERED_PRESSED_BUTTON: Color = Color::rgb(0.25, 0.65, 0.25);
+const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
+// Tag component used to mark wich setting is currently selected
+#[derive(Component)]
+struct SelectedOption;
+
+// All actions that can be triggered from a button click
+#[derive(Component)]
+enum MenuButtonAction {
+    Play,
+    Settings,
+    BackToMainMenu,
+    Quit,
+}
+
+// This system handles changing all buttons color based on mouse interaction
+fn button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor, Option<&SelectedOption>),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut color, selected) in interaction_query.iter_mut() {
+        *color = match (*interaction, selected) {
+            (Interaction::Clicked, _) => PRESSED_BUTTON.into(),
+            (Interaction::Hovered, Some(_)) => HOVERED_PRESSED_BUTTON.into(),
+            (Interaction::Hovered, None) => HOVERED_BUTTON.into(),
+            (Interaction::None, Some(_)) => PRESSED_BUTTON.into(),
+            (Interaction::None, None) => NORMAL_BUTTON.into(),
+        }
+    }
+}
+
+fn menu_setup(mut commands: Commands, mut menu_state: ResMut<State<MenuState>>) {
+    commands.spawn_bundle(UiCameraBundle::default())
+        .insert(MainMenuState);
+    let _ = menu_state.set(MenuState::Main);
+}
+
+fn main_menu_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load("fonts/Kenney Future.ttf");
+    // Common style for all buttons on the screen
+    let button_style = Style {
+        size: Size::new(Val::Px(250.0), Val::Px(65.0)),
+        margin: Rect::all(Val::Px(20.0)),
+        justify_content: JustifyContent::Center,
+        align_items: AlignItems::Center,
+        ..Default::default()
+    };
+    let button_text_style = TextStyle {
+        font: font.clone(),
+        font_size: 40.0,
+        color: TEXT_COLOR,
+    };
+
+    commands
+        .spawn_bundle(NodeBundle {
+            style: Style {
+                margin: Rect::all(Val::Auto),
+                flex_direction: FlexDirection::ColumnReverse,
+                align_items: AlignItems::Center,
+                ..Default::default()
+            },
+            color: Color::CRIMSON.into(),
+            ..Default::default()
+        })
+        .insert(OnMainMenuScreen)
+        .with_children(|parent| {
+            // Display the game name
+            parent.spawn_bundle(TextBundle {
+                style: Style {
+                    margin: Rect::all(Val::Px(50.0)),
+                    ..Default::default()
+                },
+                text: Text::with_section(
+                    "Laughing Barnacle",
+                    TextStyle {
+                        font: font.clone(),
+                        font_size: 80.0,
+                        color: TEXT_COLOR,
+                    },
+                    Default::default(),
+                ),
+                ..Default::default()
+            });
+
+            // Display three buttons for each action available from the main menu:
+            // - new game
+            // - settings
+            // - quit
+            parent
+                .spawn_bundle(ButtonBundle {
+                    style: button_style.clone(),
+                    color: NORMAL_BUTTON.into(),
+                    ..Default::default()
+                })
+                .insert(MenuButtonAction::Play)
+                .with_children(|parent| {
+                    parent.spawn_bundle(TextBundle {
+                        text: Text::with_section(
+                            "New Game",
+                            button_text_style.clone(),
+                            Default::default(),
+                        ),
+                        ..Default::default()
+                    });
+                });
+            parent
+                .spawn_bundle(ButtonBundle {
+                    style: button_style.clone(),
+                    color: NORMAL_BUTTON.into(),
+                    ..Default::default()
+                })
+                .insert(MenuButtonAction::Settings)
+                .with_children(|parent| {
+                    parent.spawn_bundle(TextBundle {
+                        text: Text::with_section(
+                            "Settings",
+                            button_text_style.clone(),
+                            Default::default(),
+                        ),
+                        ..Default::default()
+                    });
+                });
+            parent
+                .spawn_bundle(ButtonBundle {
+                    style: button_style,
+                    color: NORMAL_BUTTON.into(),
+                    ..Default::default()
+                })
+                .insert(MenuButtonAction::Quit)
+                .with_children(|parent| {
+                    parent.spawn_bundle(TextBundle {
+                        text: Text::with_section("Quit", button_text_style, Default::default()),
+                        ..Default::default()
+                    });
+                });
+        });
+}
+
+fn settings_menu_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let button_style = Style {
+        size: Size::new(Val::Px(200.0), Val::Px(65.0)),
+        margin: Rect::all(Val::Px(20.0)),
+        justify_content: JustifyContent::Center,
+        align_items: AlignItems::Center,
+        ..Default::default()
+    };
+    let button_text_style = TextStyle {
+        font: asset_server.load("fonts/Kenney Future.ttf"),
+        font_size: 40.0,
+        color: TEXT_COLOR,
+    };
+
+    commands
+        .spawn_bundle(NodeBundle {
+            style: Style {
+                margin: Rect::all(Val::Auto),
+                flex_direction: FlexDirection::ColumnReverse,
+                align_items: AlignItems::Center,
+                ..Default::default()
+            },
+            color: Color::CRIMSON.into(),
+            ..Default::default()
+        })
+        .insert(OnSettingsMenuScreen)
+        .with_children(|parent| {
+            // Display the back button to return to the main menu screen
+            parent
+                .spawn_bundle(ButtonBundle {
+                    style: button_style,
+                    color: NORMAL_BUTTON.into(),
+                    ..Default::default()
+                })
+                .insert(MenuButtonAction::BackToMainMenu)
+                .with_children(|parent| {
+                    parent.spawn_bundle(TextBundle {
+                        text: Text::with_section("Back", button_text_style, Default::default()),
+                        ..Default::default()
+                    });
+                });
+        });
+}
+
+fn menu_action(
+    interaction_query: Query<
+        (&Interaction, &MenuButtonAction),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut app_exit_events: EventWriter<AppExit>,
+    mut menu_state: ResMut<State<MenuState>>,
+    mut game_state: ResMut<State<GameState>>,
+) {
+    for (interaction, menu_button_action) in interaction_query.iter() {
+        if *interaction == Interaction::Clicked {
+            match menu_button_action {
+                MenuButtonAction::Quit => app_exit_events.send(AppExit),
+                MenuButtonAction::Play => {
+                    game_state.set(GameState::Game).unwrap();
+                    menu_state.set(MenuState::Disabled).unwrap();
+                }
+                MenuButtonAction::Settings => menu_state.set(MenuState::Settings).unwrap(),
+                MenuButtonAction::BackToMainMenu => menu_state.set(MenuState::Main).unwrap(),
+            }
+        }
+    }
+}
+
+// Generic system that takes a component as a parameter, and will despawn all entities with that component
+fn despawn_screen<T: Component>(to_despawn: Query<Entity, With<T>>, mut commands: Commands) {
+    for entity in to_despawn.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
+}
